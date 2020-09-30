bf1d0b68a710f90fa873a68e4c2f2853898c99f6
Josh Field <josh.t.field@bigpond.com>
2020-09-30 15:54:42 +0100

    semantic changes

--- addbookfunction.php
+++ addbookfunction.php
@@ -2,12 +2,14 @@
 require 'functions.php';
 $db = startDb('books');
 
-if (array_key_exists('bookName', $_POST)
-    && array_key_exists('author', $_POST)
-    && array_key_exists('category', $_POST)
-    && array_key_exists('released', $_POST)) {
-    $query = $db->prepare('INSERT INTO `booksCollected` (`name`, `author`, `category`, `released`, `deleted`) VALUES (?, ?, ?, ?, ?)');
-    if ($query->execute([$_POST['bookName'], $_POST['author'], $_POST['category'], $_POST['released'], 0])) {
+if (
+    array_key_exists('bookName', $_POST) &&
+    array_key_exists('author', $_POST) &&
+    array_key_exists('category', $_POST) &&
+    array_key_exists('released', $_POST)
+) {
+    $query = $db->prepare('INSERT INTO `booksCollected` (`name`, `author`, `category`, `released`, `deleted`) VALUES (:bookname, :author, :category, :releaseYear , :deleteStatus)');
+    if ($query->execute([':bookname' => $_POST['bookName'], ':author' => $_POST['author'], ':category' => $_POST['category'], ':releaseYear' => $_POST['released'], ':deleteStatus' => 0])) {
         header("Location: addbook.php?success=1");
     } else {
         header("Location: addbook.php?error=1");
